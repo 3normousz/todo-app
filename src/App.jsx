b7d074fefa89bcc800b2f08e59236220f787e55d
Pakapool Boonma <dinodragoz77@gmail.com>
2023-09-19 22:15:17 +0800

    Updated calendar display
    now it's being able to show correct start date, end date , and number of days depending on your current month.

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,11 @@ import Calendar from "./Calendar/Calendar"
 import './index.css'
 
 function App() {
+  const [currentDate, setCurrentDate] = useState(new Date());
   return (
     <>
       <div className='mt-16 flex flex-col items-center'>
-        <Calendar />
+        <Calendar value={currentDate} onChange={setCurrentDate} />
       </div>
     </>
 
--- src/Calendar/Calendar.jsx
+++ src/Calendar/Calendar.jsx
@@ -1,65 +1,43 @@
 import { useState } from 'react'
+import { startOfMonth, endOfMonth, differenceInDays } from 'date-fns'
 import Cell from './Cell'
 import '../index.css'
 
-const daysOfWeek = [
-    "Sun",
-    "Mon",
-    "Tue",
-    "Wed",
-    "Thu",
-    "Fri",
-    "Sat"
-];
+const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-function Calendar() {
+function Calendar({ value = new Date(), onChange }) {
+    const startDate = startOfMonth(value);
+    const endDate = endOfMonth(value);
+    const numDays = differenceInDays(endDate, startDate) + 1;
+
+    const prefixDays = startDate.getDay();
+
+    console.log(value);
+    console.log(prefixDays);
+
     return (
         <>
             <div className='w-[400px] border'>
                 <div className='grid grid-cols-7'>
                     <Cell>{"<<"}</Cell>
                     <Cell>{"<"}</Cell>
-                    <Cell className='col-span-3 font-bold'>August 2022</Cell>
+                    <Cell className='col-span-3 font-bold'>July 2022</Cell>
                     <Cell>{">"}</Cell>
                     <Cell>{">>"}</Cell>
 
                     {daysOfWeek.map((day) => (
-                        <Cell className="text-sm font-bold">
+                        <Cell key={day} className="text-sm font-bold">
                             {day}
                         </Cell>))}
 
-                    <Cell />
-                    <Cell>1</Cell>
-                    <Cell>2</Cell>
-                    <Cell>3</Cell>
-                    <Cell>4</Cell>
-                    <Cell>5</Cell>
-                    <Cell>6</Cell>
-                    <Cell>7</Cell>
-                    <Cell>8</Cell>
-                    <Cell>9</Cell>
-                    <Cell>10</Cell>
-                    <Cell>11</Cell>
-                    <Cell>12</Cell>
-                    <Cell>13</Cell>
-                    <Cell>14</Cell>
-                    <Cell>15</Cell>
-                    <Cell>16</Cell>
-                    <Cell>17</Cell>
-                    <Cell>18</Cell>
-                    <Cell>19</Cell>
-                    <Cell>20</Cell>
-                    <Cell>21</Cell>
-                    <Cell>22</Cell>
-                    <Cell>23</Cell>
-                    <Cell>24</Cell>
-                    <Cell>25</Cell>
-                    <Cell>26</Cell>
-                    <Cell>27</Cell>
-                    <Cell>28</Cell>
-                    <Cell>29</Cell>
-                    <Cell>30</Cell>
-                    <Cell>31</Cell>
+                    {Array.from({ length: prefixDays }).map((_, index) => (
+                        <Cell />
+                    ))}
+
+                    {Array.from({ length: numDays }).map((_, index) => {
+                        const date = index + 1;
+                        return <Cell>{date}</Cell>;
+                    })}
                 </div>
             </div>
         </>
