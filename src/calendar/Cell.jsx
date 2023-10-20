a3a3230cfc822c42607f76b81f7472c225ca2e7f
Pakapool Boonma <dinodragoz77@gmail.com>
2023-10-20 23:31:30 +0800

    Fixed dateCell hover:background-color overlapping each other

--- src/calendar/Cell.jsx
+++ src/calendar/Cell.jsx
@@ -6,12 +6,12 @@ import clsx from 'clsx'
 function Cell({ isEqualsToCurrentDate, isEqualsToSelectedDate, isSelectedFlag, className, onClick, isClickable = true, isDotCell = false, children }) {
     return (
         <>
-            <div className={clsx("w-14 h-14 flex items-center justify-center cursor-pointer rounded-full",
-                { "cursor-default": !isClickable },
-                { "bg-blue-500 text-white font-bold": isEqualsToCurrentDate && !isEqualsToSelectedDate && !isSelectedFlag },
+            <div className={clsx("w-14 h-14 flex items-center justify-center cursor-default rounded-full",
+                { "cursor-pointer": isClickable },
+                { "dateCellSelectedBackgroundCustomColor text-white font-bold": isEqualsToCurrentDate && !isEqualsToSelectedDate && !isSelectedFlag },
                 { "text-red-500 font-bold": isEqualsToCurrentDate && !isEqualsToSelectedDate && isSelectedFlag },
-                { "hover:bg-gray-100": !isEqualsToCurrentDate && !isEqualsToSelectedDate && isClickable && !isDotCell },
-                { "bg-blue-500 text-white font-bold": (isEqualsToSelectedDate && !isEqualsToCurrentDate) || (isEqualsToSelectedDate && isEqualsToCurrentDate) },
+                { "dateCellCustomHover": !isEqualsToSelectedDate && isClickable && !isDotCell },
+                { "dateCellSelectedBackgroundCustomColor text-white font-bold": (isEqualsToSelectedDate && !isEqualsToCurrentDate) || (isEqualsToSelectedDate && isEqualsToCurrentDate) },
                 { "pointer-events-none": isDotCell },
                 className)}
                 onClick={onClick}>
